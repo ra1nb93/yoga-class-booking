1a8b8e419f808782db7188f7ca22fee845680115
ra1nb93 <alessio-martis@hotmail.it>
2025-02-12 19:45:42 +0100

    review login.php

--- api/user/login.php
+++ api/user/login.php
@@ -4,10 +4,11 @@ ini_set('display_errors', 1);
 
 require '../../db.php';
 
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Origin: https://yoga-class-booking.netlify.app");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Credentials: true");
+
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
