6d81335ed1fe72efa24a86f3eef53ff102f6ba6a
ra1nb93 <alessio-martis@hotmail.it>
2025-02-12 14:32:50 +0100

    update db

--- db.php
+++ db.php
@@ -1,13 +1,20 @@
 <?php
 // db.php
-$host = 'localhost';
-$dbname = 'yoga_app';
+
+$host = 'monorail.proxy.rlwy.net';
+$port = '28114';   // Se serve, altrimenti puoi lasciare 3306 se in dash è riportato diversamente
+$dbname = 'railway';
 $username = 'root';
-$password = '';
+$password = 'SWmEOopHFfJeJkrDqRsXGvsPhxFMjIST';
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    // Nota l'aggiunta di `port=$port` se la porta non è la 3306
+    $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8";
+    $pdo = new PDO($dsn, $username, $password);
+
+    // Imposta il PDO in modalità eccezione
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
